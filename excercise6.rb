ca7a34662d663ebcb4fb1cc8f0834d3ac0160029
Ian Russell <ian.andrew.russell@gmail.com>
2016-10-19 16:29:16 -0400

    questions1-3 completed

--- excercise6.rb
+++ excercise6.rb
@@ -14,3 +14,16 @@ grocery(grocery_list)
 puts "looks like we need rice too"
 grocery_list.push ("rice")
 grocery(grocery_list)
+
+#outputting the total number of items on the list
+puts "Looks like we have #{grocery_list.length} items to pick up from the store."
+
+#Checking to see if the list includes bananas
+
+if grocery_list.include?("bananas") == true
+  puts "You don't need to pick up bananas."
+else
+    puts "You need to pick up bananas."
+end
+
+#displaying the 2nd item in the list
